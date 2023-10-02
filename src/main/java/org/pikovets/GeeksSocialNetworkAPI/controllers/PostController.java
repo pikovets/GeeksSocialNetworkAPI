3f9cc5af4c7b21d40aa94509b213524b48451fb2
Timur Pikovets <44881052+pikovets@users.noreply.github.com>
2023-10-02 20:59:05 +0200

    Gsn 12 add get post endpoint
    * Add get post endpoint
    
    * Describe Swagger for get post endpoint

--- src/main/java/org/pikovets/GeeksSocialNetworkAPI/controllers/PostController.java
+++ src/main/java/org/pikovets/GeeksSocialNetworkAPI/controllers/PostController.java
@@ -1,18 +1,28 @@
 package org.pikovets.GeeksSocialNetworkAPI.controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.modelmapper.ModelMapper;
 import org.pikovets.GeeksSocialNetworkAPI.dto.post.PostDTO;
+import org.pikovets.GeeksSocialNetworkAPI.exceptions.ErrorObject;
 import org.pikovets.GeeksSocialNetworkAPI.model.Post;
 import org.pikovets.GeeksSocialNetworkAPI.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.util.UUID;
+
 @RestController
 @RequestMapping("/posts")
+@Tag(name = "Post")
 public class PostController {
-    private ModelMapper modelMapper;
-    private PostService postService;
+    private final ModelMapper modelMapper;
+    private final PostService postService;
 
     @Autowired
     public PostController(ModelMapper modelMapper, PostService postService) {
@@ -20,11 +30,37 @@ public class PostController {
         this.postService = postService;
     }
 
-    public PostDTO convertToPostDTO(Post post) {
-        return modelMapper.map(post, PostDTO.class);
+    @Operation(
+            summary = "Get user by ID",
+            description = "Returns a single post",
+            parameters = {
+                    @Parameter(
+                            name = "id",
+                            description = "ID of post to return"
+                    )
+            },
+            responses = {
+                    @ApiResponse(
+                            description = "Success",
+                            responseCode = "200"
+                    ),
+                    @ApiResponse(
+                            description = "Not Found",
+                            responseCode = "404",
+                            content = @Content(schema = @Schema(implementation = ErrorObject.class))
+                    ),
+                    @ApiResponse(
+                            description = "Unauthorized / Invalid Token",
+                            responseCode = "403"
+                    )
+            }
+    )
+    @GetMapping("/{id}")
+    public PostDTO getPostById(@PathVariable("id") UUID id) {
+        return convertToPostDTO(postService.getPostById(id));
     }
 
-    public Post convertToPost(PostDTO postDTO) {
-        return modelMapper.map(postDTO, Post.class);
+    public PostDTO convertToPostDTO(Post post) {
+        return modelMapper.map(post, PostDTO.class);
     }
 }
