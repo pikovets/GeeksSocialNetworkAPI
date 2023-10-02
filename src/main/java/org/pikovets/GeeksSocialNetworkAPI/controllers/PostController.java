d3efb01c72c4e63050f1f627d729d32cc9347ffd
Timur Pikovets <pikovets.t@gmail.com>
2023-10-02 20:55:48 +0200

    Describe Swagger for get post endpoint

--- src/main/java/org/pikovets/GeeksSocialNetworkAPI/controllers/PostController.java
+++ src/main/java/org/pikovets/GeeksSocialNetworkAPI/controllers/PostController.java
@@ -19,6 +19,7 @@ import java.util.UUID;
 
 @RestController
 @RequestMapping("/posts")
+@Tag(name = "Post")
 public class PostController {
     private final ModelMapper modelMapper;
     private final PostService postService;
@@ -29,6 +30,31 @@ public class PostController {
         this.postService = postService;
     }
 
+    @Operation(
+            summary = "Get user by ID",
+            description = "Returns a single post",
+            parameters = {
+                    @Parameter(
+                            name = "id",
+                            description = "ID of post to return"
+                    )
+            },
+            responses = {
+                    @ApiResponse(
+                            description = "Success",
+                            responseCode = "200"
+                    ),
+                    @ApiResponse(
+                            description = "Not Found",
+                            responseCode = "404",
+                            content = @Content(schema = @Schema(implementation = ErrorObject.class))
+                    ),
+                    @ApiResponse(
+                            description = "Unauthorized / Invalid Token",
+                            responseCode = "403"
+                    )
+            }
+    )
     @GetMapping("/{id}")
     public PostDTO getPostById(@PathVariable("id") UUID id) {
         return convertToPostDTO(postService.getPostById(id));
